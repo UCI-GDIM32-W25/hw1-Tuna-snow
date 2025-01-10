e51957e7cb2c7f85db2a38088aff5094d234944f
Tuyet Kha Bui <tuyetkha.bui2005@gmail.com>
2025-01-10 02:17:36 -0800

    first push

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,8 +6,9 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
-    public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    public void UpdateSeeds(int seedsLeft, int seedsPlanted)
     {
-        
+        _plantedText.text = "Seeds Planted: " + seedsPlanted.ToString();
+        _remainingText.text = "Seeds Remaining: " + seedsLeft.ToString();
     }
 }
